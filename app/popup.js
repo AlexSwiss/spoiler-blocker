7331026f28950c9178157d2d02ddcf2bc097ab7b
alexswiss <alexikeh@gmail.com>
2020-05-29 19:07:14 +0100

    added tab sorting func

--- app/popup.js
+++ app/popup.js
@@ -1,8 +1,19 @@
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-  document.write(`<h3>The tabs you're on are:</h3>`);
-  document.write('<ul>');
-  for (let i = 0; i < tabs.length; i++) {
-    document.write(`<li>${tabs[i].url}</li>`);
+function byAlphabeticalURLOrder(tab1, tab2) {
+  if (tab1.url < tab2.url) {
+    return -1;
+  } else if (tab1.url > tab2.url) {
+    return 1;
   }
-  document.write('</ul>');
-});
+  return 0;
+}
+
+  chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+      tabs.sort(byAlphabeticalURLOrder);
+    
+        document.write(`<h3>The tabs you're on are:</h3>`);
+        document.write('<ul>');
+        for (let i = 0; i < tabs.length; i++) {
+          document.write(`<li>${tabs[i].url}</li>`);
+        }
+        document.write('</ul>');
+      });
