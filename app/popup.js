91a9718a6ad1c81c4cc5aecf53e3c44da0280754
alexswiss <alexikeh@gmail.com>
2020-05-29 19:16:54 +0100

    finished sorting tabs

--- app/popup.js
+++ app/popup.js
@@ -9,11 +9,8 @@ function byAlphabeticalURLOrder(tab1, tab2) {
 
   chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
       tabs.sort(byAlphabeticalURLOrder);
-    
-        document.write(`<h3>The tabs you're on are:</h3>`);
-        document.write('<ul>');
-        for (let i = 0; i < tabs.length; i++) {
-          document.write(`<li>${tabs[i].url}</li>`);
-        }
-        document.write('</ul>');
+      for (let i = 0; i < tabs.length; i++) {
+             chrome.tabs.move(tabs[i].id, {index: i});
+           }
+      
       });
